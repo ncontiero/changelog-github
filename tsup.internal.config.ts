b798eba635b573c1833c836d0457fb25f59ffdb1
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-11-04 19:02:48 -0300

    update: moving copy file command to tsup.internal

--- tsup.internal.config.ts
+++ tsup.internal.config.ts
@@ -5,6 +5,7 @@ import { configOptions as baseConfigOptions } from "./tsup.config";
 const configOptions: Options = {
   ...baseConfigOptions,
   outDir: "node_modules/@dkshs/changelog-github/dist",
+  onSuccess: "copyfiles ./package.json ./node_modules/@dkshs/changelog-github",
 };
 
 export default defineConfig(configOptions);
